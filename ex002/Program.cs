5b9f29851154cc509dbc0a3a86bac13c09739417
MisterGrad <mistergrad@icloud.com>
2022-09-01 03:41:38 +0500

    003

--- ex002/Program.cs
+++ ex002/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! EX002");
+//Console.WriteLine("Hello, World! EX002");
+Console.WriteLine("Привет!");
 
