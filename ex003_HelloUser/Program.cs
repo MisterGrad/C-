261f1865ee2c9c4e6df58b8c739c2fd732d3421e
MisterGrad <mistergrad@icloud.com>
2022-09-01 14:58:32 +0500

    006

--- ex003_HelloUser/Program.cs
+++ ex003_HelloUser/Program.cs
@@ -1,10 +1,16 @@
-﻿Console.Write("Введите имя -");
+﻿/*Console.Write("Введите имя - ");
 string user_name = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(user_name);
 Console.Clear();
+*/
 
 
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
 
 
 
